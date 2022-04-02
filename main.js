13baf3ede566a0e0ca03d42ffb132a18c583e7ba
tomi torres <tomitorres.93@gmail.com>
2022-04-02 18:55:07 -0300

    pricing

--- main.js
+++ main.js
@@ -188,8 +188,8 @@ cssbutton.addEventListener('click', function () {
 
 jsbutton.addEventListener('click', function () {
 
-    nombre.classList.toggle('hide');
-    nombre.classList.toggle('upward');
+    darkmode.classList.toggle('hide');
+    darkmode.classList.toggle('upward');
 
     
 })
@@ -197,38 +197,332 @@ jsbutton.addEventListener('click', function () {
 
 
 
-
-// $('#nombre').bind('keyup change',function(){
-//     if(this.value.length > 0){
-//       $('#botonsubmit').show();
-
-// }
-// else {
-//     $('#botonsubmit').hide();
-// }
-// });
+//// DARKMODE
+const darkmode = document.querySelector('#darkmode')
+const sol = document.querySelector('#sol')
+const luna = document.querySelector('#luna')
 
 
-// const inputnombre = document.querySelector('#inputnombre')
+darkmode.addEventListener('click', function () {
 
-// let form = document.getElementById('form');
+    bio2.classList.toggle('whitemode');
+    luna.classList.toggle('hide');
+    luna.classList.toggle('upward');
+    sol.classList.toggle('hide');
+    sol.classList.toggle('upward');
+    darkmode.classList.toggle('darkmodeback');
 
-// form.addEventListener("input", function(e) {
-//     e.preventDefault();
+})
+
+
+
+
+    ///////////////////////
+    ////BOTONES PRECIOS////
+    ///////////////////////
+
+    const botonpesos = document.querySelector('#botonpesos')
+    const botonstable = document.querySelector('#botonstable')
+    const botoneth = document.querySelector('#botoneth')
+    const botonbtc = document.querySelector('#botonbtc')
+
+    const preciofinalpesos = document.querySelector('#preciofinalpesos')
+    const preciofinalusd = document.querySelector('#preciofinalusd')
+    const preciofinalethcont = document.querySelector('#preciofinalethcont')
+    const preciofinalbtccont = document.querySelector('#preciofinalbtccont')
+
+
+
+
+    function elegirprecio(a, b, c, d, e, f, g, h) {
+
+    a.addEventListener('click', function () {
+
+        a.classList.add('monedaactiva');
+        b.classList.remove('hide');
+        b.classList.toggle('side');
+
+        c.classList.add('hide');
+        
+
+        d.classList.add('hide');
+
+        e.classList.add('hide');
+
+        f.classList.remove('monedaactiva');
+        g.classList.remove('monedaactiva');
+        h.classList.remove('monedaactiva');
+
+
     
-//     console.log(nombre.value)
-// })
+    })}
+
+    let elegirpesos = elegirprecio(botonpesos, preciofinalpesos, preciofinalusd, preciofinalethcont, preciofinalbtccont, botonstable, botoneth, botonbtc)
+
+    let elegirusd = elegirprecio(botonstable, preciofinalusd, preciofinalpesos,  preciofinalethcont, preciofinalbtccont, botonpesos, botoneth, botonbtc)
+
+    let elegireth = elegirprecio(botoneth, preciofinalethcont, preciofinalpesos, preciofinalusd,  preciofinalbtccont, botonpesos, botonstable, botonbtc)
+
+    let elegirbtc = elegirprecio(botonbtc, preciofinalbtccont, preciofinalpesos, preciofinalusd, preciofinalethcont, botonpesos, botonstable, botoneth)
 
 
-  
 
-// // `${nuevaSolicitud.nombre}`
 
-// const confia = document.querySelector('#confia')
+    ////////////////////////////////
+////////SECCION PRECIOS/////////
+////////////////////////////////
+
+const landing = document.querySelector('#landing')
+const multipage = document.querySelector('#multipage')
+const paginas = document.querySelector('#paginas')
+const cincopag = document.querySelector('#cincopag')
+const diezpag = document.querySelector('#diezpag')
+
+// const precioeth = document.querySelector('#precioeth')
+// const preciobtc = document.querySelector('#preciobtc')
+// const preciousd = document.querySelector('#preciousd')
+// const preciopesos = document.querySelector('#preciopesos')
+
+
+
+const preciopesoscinco = document.querySelector('#preciopesoscinco')
+const preciopesosdiez = document.querySelector('#preciopesosdiez')
+
+const preciousdcinco = document.querySelector('#preciousdcinco')
+const preciousddiez = document.querySelector('#preciousddiez')
+
+const precioethcinco = document.querySelector('#precioethcinco')
+const precioethdiez = document.querySelector('#precioethdiez')
+
+const preciobtccinco = document.querySelector('#preciobtccinco')
+const preciobtcdiez = document.querySelector('#preciobtcdiez')
+
+
+multipage.addEventListener('click', function () {
+    paginas.classList.toggle('side');
+    paginas.classList.toggle('hide');
+    
+})
+
+landing.addEventListener('click', function () {
+    paginas.classList.add('hide');
+    
+})
+
+function cambiarPrecio (a,b,c,d,e,f,g,h,i,j,k,l,m) {
+
+    a.addEventListener('click', function () {
+        
+        b.classList.add('hide');
+        c.classList.add('hide');
+        d.classList.add('hide');
+        e.classList.add('hide');
+        f.classList.add('hide');
+        g.classList.add('hide');
+        h.classList.add('hide');
+        i.classList.add('hide');
+
+        j.classList.remove('hide');
+        k.classList.remove('hide');
+        l.classList.remove('hide');
+        m.classList.remove('hide');
+
+
+
+    })
+    
+}
+
+let cambiarLanding = cambiarPrecio (landing,preciopesoscinco,preciopesosdiez,preciousdcinco,preciousddiez,precioethcinco,precioethdiez,preciobtccinco,preciobtcdiez, precioeth,preciobtc,preciousd,preciopesos)
+
+
+let cambiarMultiC = cambiarPrecio (cincopag,preciopesos,preciopesosdiez,preciousd,preciousddiez,precioeth,precioethdiez,preciobtc,preciobtcdiez, precioethcinco,preciobtccinco,preciousdcinco,preciopesoscinco)
+
+let cambiarMultiD = cambiarPrecio (diezpag,preciopesoscinco,preciopesos,preciousdcinco,preciousd,precioethcinco,precioeth,preciobtccinco,preciobtc, precioethdiez,preciobtcdiez,preciousddiez,preciopesosdiez)
+
+
+
+
+
+
+/////// COTIZACIONES
+
+//ETH 
+setTimeout(function() {
+    precioeth = document.getElementById("precioeth")
+    
+    document.getElementById("precioeth")
+    fetch('https://criptoya.com/api/eth/usd/0.1')
+    .then(response => response.json())
+    .then(data => {
+        let ethPrice = data.bitex.ask;
+        $("#precioeth").append(`
+        <span> <strong> ${(99/ethPrice).toFixed(3)}</strong> </span>
+     `)
+        
+    });
+
+
+    }, 10);
+
+   //ETH 
+setTimeout(function() {
+    precioeth = document.getElementById("precioeth")
+    
+    document.getElementById("precioeth")
+    fetch('https://criptoya.com/api/eth/usd/0.1')
+    .then(response => response.json())
+    .then(data => {
+        let ethPrice = data.bitex.ask;
+        $("#precioethcinco").append(`
+        <span> <strong> ${(119/ethPrice).toFixed(3)}</strong> </span>
+     `)
+        
+    });
+
+
+    }, 10);
+    
+    //ETH 
+setTimeout(function() {
+    precioeth = document.getElementById("precioeth")
+    
+    document.getElementById("precioeth")
+    fetch('https://criptoya.com/api/eth/usd/0.1')
+    .then(response => response.json())
+    .then(data => {
+        let ethPrice = data.bitex.ask;
+        $("#precioethdiez").append(`
+        <span> <strong> ${(139/ethPrice).toFixed(3)}</strong> </span>
+     `)
+        
+    });
+
+
+    }, 10);
+
+    
+    //BTC 
+setTimeout(function() {
+    preciobtc = document.getElementById("preciobtc")
+    
+    document.getElementById("preciobtc")
+    fetch('https://criptoya.com/api/btc/usd/0.1')
+    .then(response => response.json())
+    .then(data => {
+        let btcPrice = data.bitex.ask;
+        $("#preciobtc").append(`
+        <span> <strong> ${(99/btcPrice).toFixed(4)}</strong> </span>
+     `)
+        
+    });
+
+
  
+    }, 10);
 
-// botonsubmit.addEventListener('click', function () {
+        //BTC  5
+setTimeout(function() {
+    preciobtc = document.getElementById("preciobtc")
+    
+    document.getElementById("preciobtc")
+    fetch('https://criptoya.com/api/btc/usd/0.1')
+    .then(response => response.json())
+    .then(data => {
+        let btcPrice = data.bitex.ask;
+        $("#preciobtccinco").append(`
+        <span> <strong> ${(119/btcPrice).toFixed(4)}</strong> </span>
+     `)
+        
+    });
 
-//     confia2.innerHTML = nombre.value;
-//     confia.classList.toggle('hide');
-// })
+
+ 
+    }, 10);
+
+        //BTC 10
+setTimeout(function() {
+    preciobtc = document.getElementById("preciobtc")
+    
+    document.getElementById("preciobtc")
+    fetch('https://criptoya.com/api/btc/usd/0.1')
+    .then(response => response.json())
+    .then(data => {
+        let btcPrice = data.bitex.ask;
+        $("#preciobtcdiez").append(`
+        <span> <strong> ${(139/btcPrice).toFixed(4)}</strong> </span>
+     `)
+        
+    });
+
+
+ 
+    }, 10);
+
+
+
+        //PESOS - LANDING
+        setTimeout(function preciopesosf() {
+                preciopesos = document.getElementById("preciopesos")
+                
+            document.getElementById("preciobtc")
+                fetch('https://criptoya.com/api/dolar')
+                .then(response => response.json())
+                .then(data => {
+                    let usdPrice = data.solidario;
+                   
+            
+                    $("#preciopesos").append(`
+                    <span id="ppp"> <strong> ${(99*usdPrice).toFixed(0)}</strong> </span>
+                 `)
+                    
+                });
+            
+            
+            
+                }, 10);
+            
+        //PESOS - MULTI 5
+        setTimeout(function preciopesosf() {
+            preciopesos = document.getElementById("preciopesos")
+            
+        document.getElementById("preciobtc")
+            fetch('https://criptoya.com/api/dolar')
+            .then(response => response.json())
+            .then(data => {
+                let usdPrice = data.solidario;
+               
+        
+                $("#preciopesoscinco").append(`
+                <span> <strong> ${(119*usdPrice).toFixed(0)}</strong> </span>
+             `)
+                
+            });
+        
+        
+        
+            }, 10);
+
+                    
+        //PESOS - MULTI 10
+        setTimeout(function preciopesosf() {
+            preciopesos = document.getElementById("preciopesos")
+            
+        document.getElementById("preciobtc")
+            fetch('https://criptoya.com/api/dolar')
+            .then(response => response.json())
+            .then(data => {
+                let usdPrice = data.solidario;
+               
+        
+                $("#preciopesosdiez").append(`
+                <span> <strong> ${(139*usdPrice).toFixed(0)}</strong> </span>
+             `)
+                
+            });
+        
+        
+        
+            }, 10);
+            
+        
